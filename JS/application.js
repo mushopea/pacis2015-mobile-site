d60ad4d994a8f145d211bf045438628e2f30c6d1
mushopea <mushopea@hotmail.com>
2015-06-26 20:35:25 +0800

    enable swipe

--- JS/application.js
+++ JS/application.js
@@ -3,39 +3,41 @@
 	application.js
 */
 $(document).on("pageinit", function(event){
+  console.log("page init!");
 	// custom code goes here
-
-
-
 });
 
 // Pagecreate will fire for each of the pages in this demo
 // but we only need to bind once so we use "one()"
-$( document ).one( "pagecreate", "home", function() {
+$( document ).one( "pagecreate", "body", function() {
+  console.log("page create!");
     // Handler for navigating to the next page
     function navnext( next ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", next + ".html", {
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#" + next, {
             transition: "slide"
         });
     }
     // Handler for navigating to the previous page
     function navprev( prev ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", prev + ".html", {
+      console.log("changing!");
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#" + prev, {
             transition: "slide",
             reverse: true
         });
     }
     // Navigate to the next page on swipeleft
     $( document ).on( "swipeleft", ".ui-page", function( event ) {
+      console.log("hi");
         // Get the filename of the next page. We stored that in the data-next
         // attribute in the original markup.
         var next = $( this ).jqmData( "next" );
+        console.log(next);
         // Check if there is a next page and
         // swipes may also happen when the user highlights text, so ignore those.
         // We're only interested in swipes on the page.
-        if ( next && ( event.target === $( this )[ 0 ] ) ) {
+        // if ( next && ( event.target === $( this )[ 0 ] ) ) {
             navnext( next );
-        }
+        // }
     });
     // Navigate to the next page when the "next" button in the footer is clicked
     $( document ).on( "click", ".next", function() {
@@ -48,9 +50,9 @@ $( document ).one( "pagecreate", "home", function() {
     // The same for the navigating to the previous page
     $( document ).on( "swiperight", ".ui-page", function( event ) {
         var prev = $( this ).jqmData( "prev" );
-        if ( prev && ( event.target === $( this )[ 0 ] ) ) {
+        //if ( prev && ( event.target === $( this )[ 0 ] ) ) {
             navprev( prev );
-        }
+        //}
     });
     $( document ).on( "click", ".prev", function() {
         var prev = $( ".ui-page-active" ).jqmData( "prev" );
