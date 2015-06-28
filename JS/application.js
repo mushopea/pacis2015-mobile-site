f2412f2086b10e62741fa1ac1ac5e5dadb1a1230
mushopea <mushopea@hotmail.com>
2015-06-29 00:20:56 +0800

    accordion animation and scroll

--- JS/application.js
+++ JS/application.js
@@ -4,21 +4,21 @@
  */
 
 // Handler for navigating to the next page
-function navnext( next ) {
-  $( ":mobile-pagecontainer" ).pagecontainer( "change", "#" + next, {
+function navnext(next) {
+  $(":mobile-pagecontainer").pagecontainer("change", "#" + next, {
     transition: "slide"
   });
 }
 
 // Handler for navigating to the previous page
-function navprev( prev ) {
-  $( ":mobile-pagecontainer" ).pagecontainer( "change", "#" + prev, {
+function navprev(prev) {
+  $(":mobile-pagecontainer").pagecontainer("change", "#" + prev, {
     transition: "slide",
     reverse: true
   });
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $(".panzoom").panzoom();
 
   $("#programsdate").change(function() {
@@ -28,47 +28,70 @@ $(document).ready(function(){
   });
 });
 
+$(document).on("pagecreate", function() {
+
+    $('#accordion').bind('accordionactivate', function(event, ui) {
+      /* In here, ui.newHeader = the newly active header as a jQ object
+                  ui.newContent = the newly active content area */
+      $(ui.newHeader).ScrollTo();
+      console.log("scrolling to new header of accordion");
+    });
+
+    // animate accordion
+    $(".accordion-section .ui-collapsible-heading-toggle").on("click", function(e) {
+      var current = $(this).closest(".ui-collapsible");
+      if (current.hasClass("ui-collapsible-collapsed")) {
+        //collapse all others and then expand this one
+        console.log("expanding and animating accordion");
+        $(".ui-collapsible").not(".ui-collapsible-collapsed").find(".ui-collapsible-heading-toggle").click();
+        $(".ui-collapsible-content", current).slideDown(300);
+      } else {
+        $(".ui-collapsible-content", current).slideUp(300);
+      }
+    });
+});
+
 // Pagecreate will fire for each of the pages in this demo
 // but we only need to bind once so we use "one()"
-$( document ).one( "pagecreate", "body", function() {
+$(document).one("pagecreate", "body", function() {
   // Navigate to the next page on swipeleft
-  $( document ).on( "swipeleft", ".ui-page", function( event ) {
-    var next = $( this ).jqmData( "next" );
-    navnext( next );
+  $(document).on("swipeleft", ".ui-page", function(event) {
+    var next = $(this).jqmData("next");
+    navnext(next);
   });
 
   // Navigate to the next page on swiperight
-  $( document ).on( "swiperight", ".ui-page", function( event ) {
-    var prev = $( this ).jqmData( "prev" );
-    navprev( prev );
+  $(document).on("swiperight", ".ui-page", function(event) {
+    var prev = $(this).jqmData("prev");
+    navprev(prev);
   });
 });
 
-$( document ).on( "pageshow", ".demo-page", function() {
-  var thePage = $( this ),
-      title = thePage.jqmData( "title" ),
-      next = thePage.jqmData( "next" ),
-      prev = thePage.jqmData( "prev" );
+$(document).on("pageshow", ".demo-page", function() {
+  var thePage = $(this),
+    title = thePage.jqmData("title"),
+    next = thePage.jqmData("next"),
+    prev = thePage.jqmData("prev");
   // Point the "Trivia" button to the popup for the current page.
-  $( "#trivia-button" ).attr( "href", "#" + thePage.find( ".trivia" ).attr( "id" ) );
+  $("#trivia-button").attr("href", "#" + thePage.find(".trivia").attr("id"));
   // We use the same header on each page
   // so we have to update the title
-  $( "#header h1" ).text( title );
+  $("#header h1").text(title);
   // Prefetch the next page
   // We added data-dom-cache="true" to the page so it won't be deleted
   // so there is no need to prefetch it
-  if ( next ) {
-    $( ":mobile-pagecontainer" ).pagecontainer( "load", next + ".html" );
+  if (next) {
+    $(":mobile-pagecontainer").pagecontainer("load", next + ".html");
   }
   // We disable the next or previous buttons in the footer
   // if there is no next or previous page
   // We use the same footer on each page
   // so first we remove the disabled class if it is there
-  $( ".next.ui-state-disabled, .prev.ui-state-disabled" ).removeClass( "ui-state-disabled" );
-  if ( ! next ) {
-    $( ".next" ).addClass( "ui-state-disabled" );
+  $(".next.ui-state-disabled, .prev.ui-state-disabled").removeClass("ui-state-disabled");
+  if (!next) {
+    $(".next").addClass("ui-state-disabled");
   }
-  if ( ! prev ) {
-    $( ".prev" ).addClass( "ui-state-disabled" );
+  if (!prev) {
+    $(".prev").addClass("ui-state-disabled");
   }
 });
